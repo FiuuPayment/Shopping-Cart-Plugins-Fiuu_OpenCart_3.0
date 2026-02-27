a7f1439e3e49f9d1815421fe81e86649eaaab07f
sarahhghazali <sarah.ghazali@razer.com>
2026-02-27 16:08:22 +0800

    unzip opencart3.0.10_seamless

---
+++ opencart3.0.10_seamless/upload/catalog/language/en-gb/extension/payment/molpay.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Razer Merchant Services OpenCart Plugin
+ * 
+ * @package Payment Gateway
+ * @author Razer Merchant Services Technical Team <technical-sa@razer.com>
+ * @version 3.0.10
+ */
+ 
+// Text
+$_['text_title'] = 'Fiuu (Razer Merchant Service)';
+
+// Channel
+$_['channel_list']					= 	array(
+											'affinonline' 	=> 'Affin Bank(Affin Online)',
+											'amb' 			=> 'Am Bank (Am Online)',
+											'bankislam' 	=> 'Bank Islam',
+											'cimbclicks' 	=> 'CIMB Bank(CIMB Clicks)',
+											'hlb' 			=> 'Hong Leong Bank(HLB Connect)',
+											'maybank2u' 	=> 'Maybank(Maybank2u)',
+											'pbb' 			=> 'PublicBank (PBB Online)',
+											'rhb' 			=> 'RHB Bank(RHB Now)',
+											'fpx_mb2u' 			=> 'FPX Maybank(Maybank2u)',
+											'fpx_cimbclicks' 	=> 'FPX CIMB Bank(CIMB Clicks)',
+											'fpx_abb' 			=> 'FPX Affin Bank Berhad',
+											'fpx_abmb' 			=> 'FPX Alliance Bank (Alliance Online)',
+											'fpx_amb' 			=> 'FPX Am Bank (Am Online)',
+											'fpx_bimb' 			=> 'FPX Bank Islam',
+											'fpx_bkrm' 			=> 'FPX Bank Kerjasama Rakyat Malaysia',
+											'fpx_bmmb' 			=> 'FPX Bank Muamalat',
+											'fpx_bsn' 			=> 'FPX Bank Simpanan Nasional (myBSN)',
+											'fpx_hlb' 			=> 'FPX Hong Leong Bank(HLB Connect)',
+											'fpx_hsbc' 			=> 'FPX Hongkong and Shanghai Banking Corporation',
+											'fpx_kfh' 			=> 'FPX Kuwait Finance House',
+											'fpx_ocbc' 			=> 'FPX OCBC Bank',
+											'fpx_pbb' 			=> 'FPX PublicBank (PBB Online)',
+											'fpx_rhb' 			=> 'FPX RHB Bank(RHB Now)',
+											'fpx_scb' 			=> 'FPX Standard Chartered Bank	',
+											'fpx_uob' 			=> 'FPX United Overseas Bank (UOB)',
+											'cash-711' 		=> '7-Eleven(Razer Cash)',
+											'ATMVA' 		=> 'ATM Transfer via Permata Bank',
+											'dragonpay' 	=> 'Dragonpay',
+											'paysbuy' 		=> 'PaysBuy',
+											'creditAN' 		=> 'Multi Currency Credit Card',
+											'NGANLUONG' 	=> 'NGANLUONG',
+											'crossborder' 	=> 'Credit Card/ Debit Card (PayVision)',
+											'BOOST' 			=> 'BOOST',
+											'MB2U_QRPay-Push' 	=> 'Maybank QRPay',
+											'GrabPay' 			=> 'GrabPay',
+											'TNG-EWALLET' 		=> 'Touch \'N Go',
+											'WeChatPayMY' 		=> 'WeChat Pay MY',
+											'ShopeePay' 		=> 'Shopee Pay',
+											'Atome' 			=> 'Atome',
+											'GUPOP' 		    => 'UnionPay',
+											'alipay' 		    => 'Alipay',   
+										);
