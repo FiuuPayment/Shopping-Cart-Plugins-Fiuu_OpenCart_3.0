5d174c10e03434d561c0f9f681cecc6f74825dda
sarahhghazali <sarah.ghazali@razer.com>
2026-03-02 09:54:58 +0800

    unzip opencart3.0_seamless_wJournal3.0.7

---
+++ opencart3.0_seamless_wJournal3.0.7/upload/admin/controller/extension/payment/molpay.php
@@ -0,0 +1,253 @@
+<?php
+/**
+ * MOLPay OpenCart Plugin
+ *
+ * @package Payment Gateway
+ * @author MOLPay Technical Team <technical@molpay.com>
+ * @version 3.0
+ */
+
+class ControllerExtensionPaymentMolpay extends Controller {
+        private $error = array();
+
+        public function index() {
+
+                $this->load->language('extension/payment/molpay');
+
+
+                $this->document->setTitle($this->language->get('heading_title'));
+
+                $this->load->model('setting/setting');
+
+                if (($this->request->server['REQUEST_METHOD'] == 'POST') &&
+                $this->validate()) {
+                        // echo "<pre>";print_r($this->request->post);exit;
+
+                        $this->model_setting_setting->editSetting('payment_molpay', $this->request->post);
+
+                        $this->session->data['success'] = $this->language->get('text_success');
+
+                        $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
+                }
+
+
+                $data['heading_title'] = $this->language->get('heading_title');
+
+                $data['text_edit'] = $this->language->get('text_edit');
+                $data['text_enabled'] = $this->language->get('text_enabled');
+                $data['text_disabled'] = $this->language->get('text_disabled');
+                $data['text_all_zones'] = $this->language->get('text_all_zones');
+                $data['text_yes'] = $this->language->get('text_yes');
+                $data['text_no'] = $this->language->get('text_no');
+
+                $data['entry_mid'] = $this->language->get('entry_mid');
+                $data['entry_vkey'] = $this->language->get('entry_vkey');
+                $data['entry_skey'] = $this->language->get('entry_skey');
+                $data['entry_type'] = $this->language->get('entry_type');
+                $data['entry_order_status'] = $this->language->get('entry_order_status');
+                $data['entry_completed_status'] = $this->language->get('entry_completed_status');
+                $data['entry_pending_status'] = $this->language->get('entry_pending_status');
+                $data['entry_failed_status'] = $this->language->get('entry_failed_status');
+                $data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
+                $data['entry_status'] = $this->language->get('entry_status');
+                $data['entry_sort_order'] = $this->language->get('entry_sort_order');
+                $data['molpay_url'] = parse_url (HTTP_SERVER);
+
+                //Load all channel language file.
+                $data['channel_list'] = $this->language->get('channel_list');
+
+                $data['help_vkey'] = $this->language->get('help_vkey');
+                $data['help_skey'] = $this->language->get('help_skey');
+                $data['help_type'] = $this->language->get('help_type');
+
+                $data['button_save'] = $this->language->get('button_save');
+                $data['button_cancel'] = $this->language->get('button_cancel');
+
+                if (isset($this->error['warning'])) {
+                        $data['error_warning'] = $this->error['warning'];
+                } else {
+                        $data['error_warning'] = '';
+                }
+
+                if (isset($this->error['mid'])) {
+                        $data['error_mid'] = $this->error['mid'];
+                } else {
+                        $data['error_mid'] = '';
+                }
+
+                if (isset($this->error['vkey'])) {
+                        $data['error_vkey'] = $this->error['vkey'];
+                } else {
+                        $data['error_vkey'] = '';
+                }
+
+                if (isset($this->error['skey'])) {
+                        $data['error_skey'] = $this->error['skey'];
+                } else {
+                        $data['error_skey'] = '';
+                }
+
+		if (isset($this->error['type'])) {
+                        $data['error_type'] = $this->error['type'];
+                } else {
+                        $data['error_type'] = '';
+                }
+
+
+                $data['breadcrumbs'] = array();
+
+                $data['breadcrumbs'][] = array(
+                        'text' => $this->language->get('text_home'),
+						'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+                );
+
+                $data['breadcrumbs'][] = array(
+                        'text' => $this->language->get('text_extension'),
+                        // 'href' => $this->url->link('extension/extension',
+                        // 'user_token=' .
+                        // $this->session->data['user_token'] .
+                        // '&type=payment', true)
+                        'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true)
+                );
+
+                $data['breadcrumbs'][] = array(
+                        'text' => $this->language->get('heading_title'),
+                        'href' => $this->url->link('extension/payment/molpay', 'user_token=' . $this->session->data['user_token'], true)
+                                );
+
+                $data['action'] = $this->url->link('extension/payment/molpay', 'user_token=' . $this->session->data['user_token'], true);
+
+                // $data['cancel'] = $this->url->link('extension/extension',
+                // 'user_token=' . $this->session->data['user_token'] .
+                // '&type=payment', true);
+                $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true);
+
+                if (isset($this->request->post['payment_molpay_mid'])) {
+                        $data['payment_molpay_mid'] = $this->request->post['payment_molpay_mid'];
+                } else {
+                        $data['payment_molpay_mid'] = $this->config->get('payment_molpay_mid');
+                }
+                if (isset($this->request->post['payment_molpay_vkey'])) {
+                        $data['payment_molpay_vkey'] = $this->request->post['payment_molpay_vkey'];
+                } else {
+                        $data['payment_molpay_vkey'] = $this->config->get('payment_molpay_vkey');
+                }
+
+                if (isset($this->request->post['payment_molpay_skey'])) {
+                        $data['payment_molpay_skey'] = $this->request->post['payment_molpay_skey'];
+                } else {
+                        $data['payment_molpay_skey'] = $this->config->get('payment_molpay_skey');
+                }
+          
+                if (isset($this->request->post['payment_molpay_type'])) {
+                        $data['payment_molpay_type'] = $this->request->post['payment_molpay_type'];
+                } else {
+                        $data['payment_molpay_type'] = $this->config->get('payment_molpay_type');
+                }
+
+                if
+                (isset($this->request->post['payment_molpay_order_status_id'])) {
+                        $data['payment_molpay_order_status_id'] = $this->request->post['payment_molpay_order_status_id'];
+                } else {
+                        $data['payment_molpay_order_status_id'] = $this->config->get('payment_molpay_order_status_id');
+                }
+
+                if
+                (isset($this->request->post['payment_molpay_completed_status_id'])) {
+                        $data['payment_molpay_completed_status_id'] = $this->request->post['payment_molpay_completed_status_id'];
+                } else {
+                        $data['payment_molpay_completed_status_id'] = $this->config->get('payment_molpay_completed_status_id');
+                }
+
+                if
+                (isset($this->request->post['payment_molpay_pending_status_id'])) {
+                        $data['payment_molpay_pending_status_id'] = $this->request->post['payment_molpay_pending_status_id'];
+                } else {
+                        $data['payment_molpay_pending_status_id'] = $this->config->get('payment_molpay_pending_status_id');
+                }
+
+                if
+                (isset($this->request->post['payment_molpay_failed_status_id'])) {
+                        $data['payment_molpay_failed_status_id'] = $this->request->post['payment_molpay_failed_status_id'];
+                } else {
+                        $data['payment_molpay_failed_status_id'] = $this->config->get('payment_molpay_failed_status_id');
+                }
+
+                $this->load->model('localisation/order_status');
+
+                $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+
+                if
+                (isset($this->request->post['payment_molpay_geo_zone_id'])) {
+                        $data['payment_molpay_geo_zone_id'] = $this->request->post['payment_molpay_geo_zone_id'];
+                } else {
+                        $data['payment_molpay_geo_zone_id'] = $this->config->get('payment_molpay_geo_zone_id');
+                }
+                $this->load->model('localisation/geo_zone');
+
+                $data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+
+                if (isset($this->request->post['payment_molpay_status'])) {
+                        $data['payment_molpay_status'] = $this->request->post['payment_molpay_status'];
+                } else {
+                        $data['payment_molpay_status'] = $this->config->get('payment_molpay_status');
+                }
+
+                if
+                (isset($this->request->post['payment_molpay_sort_order'])) {
+                        $data['payment_molpay_sort_order'] = $this->request->post['payment_molpay_sort_order'];
+                } else {
+                        $data['payment_molpay_sort_order'] = $this->config->get('payment_molpay_sort_order');
+                }
+
+                //Load the data into variable
+
+                foreach($data['channel_list'] as $key=>$val)
+                {
+                        if
+                        (isset($this->request->post['payment_molpay_'.$key.'_status']))
+                        {
+                                $data['payment_molpay_'.$key.'_status'] = $this->request->post['payment_molpay_'.$key.'_status'];
+                                $data['statx'][$key] = $this->request->post['payment_molpay_'.$key.'_status'];
+                        }
+                        else
+                        {
+                                $data['payment_molpay_'.$key.'_status'] = $this->config->get('payment_molpay_'.$key.'_status');
+                                $data['statx'][$key] = $this->config->get('payment_molpay_'.$key.'_status');
+                        }
+                }
+
+                $data['header'] = $this->load->controller('common/header');
+                $data['column_left'] = $this->load->controller('common/column_left');
+                $data['footer'] = $this->load->controller('common/footer');
+
+
+                $this->response->setOutput($this->load->view('extension/payment/molpay',
+                $data));
+        }
+
+        protected function validate() {
+                if (!$this->user->hasPermission('modify', 'extension/payment/molpay')) {
+                        $this->error['warning'] = $this->language->get('error_permission');
+                }
+
+                if (!$this->request->post['payment_molpay_mid']) {
+                        $this->error['mid'] = $this->language->get('error_mid');
+                }
+
+                if (!$this->request->post['payment_molpay_vkey']) {
+                        $this->error['vkey'] = $this->language->get('error_vkey');
+                }
+
+                if (!$this->request->post['payment_molpay_skey']) {
+                        $this->error['skey'] = $this->language->get('error_skey');
+                }
+                if (!$this->request->post['payment_molpay_type']) {
+                        $this->error['type'] = $this->language->get('error_type');
+                }
+
+                return !$this->error;
+        }
+
+
+}
---
+++ opencart3.0_seamless_wJournal3.0.7/upload/catalog/controller/extension/payment/molpay.php
@@ -0,0 +1,281 @@
+<?php
+/**
+ * Razer Merchant Services OpenCart Plugin
+ *
+ * @package Payment Gateway
+ * @author Razer Merchant Services Technical Team <technical-sa@razer.com>
+ * @version 2.0
+ */
+
+class ControllerExtensionPaymentMolpay extends Controller {
+    public function index() {
+        $data['button_confirm'] = $this->language->get('button_confirm');
+
+        $this->load->model('checkout/order');
+
+        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+
+        $data['action'] = $this->config->get('payment_molpay_type').'MOLPay/pay/'.$this->config->get('payment_molpay_mid').'/';
+        $data['mid']= $this->config->get('payment_molpay_mid');
+        $data['amount'] = str_replace(',', '',number_format($this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false),2));
+        $data['orderid'] = $this->session->data['order_id'];
+        $data['bill_name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
+        $data['bill_email'] =  $order_info['email'];
+        $data['bill_mobile'] = $order_info['telephone'];
+        $data['country'] = $order_info['payment_iso_code_2'];
+        $data['currency'] = $order_info['currency_code'];
+        $data['vcode'] = md5($data['amount'].$this->config->get('payment_molpay_mid').$data['orderid'].$this->config->get('payment_molpay_vkey'));
+        $data['js'] = $this->config->get('payment_molpay_type').'MOLPay/API/seamless/latest/js/MOLPay_seamless.deco.js';
+
+        //Load all channel from language file.
+        $this->load->language('extension/payment/molpay');
+        $channel_list = $this->language->get('channel_list');
+
+        foreach($channel_list as $key=>$val)
+        {
+            $inGet = 'payment_molpay_'.$key.'_status';
+            $data['channel_list'][$key] = $this->config->get($inGet);
+        }
+
+        $products = $this->cart->getProducts();
+        $data['prod_desc2']="";
+            
+        foreach ($products as $product) {
+            $data['prod_desc2'].= $product['name']." x ".$product['quantity']."\n";
+        }
+
+        $data['lang'] = $this->session->data['language'];
+
+        $data['returnurl'] = $this->url->link('extension/payment/molpay/return_ipn', '', 'SSL');
+        $data['cancelurl'] = $this->url->link('extension/payment/molpay/cancelOrder');
+
+        $version_oc = substr(VERSION,0,3);
+
+        if($version_oc == "2.3")
+        {
+            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/extension/payment/molpay.twig')) {
+            return $this->load->view($this->config->get('config_template') . '/template/extension/payment/molpay.twig', $data);
+            } else {
+                return $this->load->view('extension/payment/molpay', $data);
+            }
+        }
+        else
+        {
+            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/extension/payment/molpay')) {
+            return $this->load->view($this->config->get('config_template') . '/template/extension/payment/molpay', $data);
+            } else {
+                return $this->load->view('default/template/extension/payment/molpay', $data);
+            }
+        }
+    }
+
+    //cancel order when close credit card popup
+    public function cancelOrder() {
+        
+        unset($this->session->data['language']);
+        unset($this->session->data['order_id']);
+            
+        echo '<html>' . "\n";
+        echo '<head>' . "\n";
+        echo '  <meta http-equiv="Refresh" content="0; url=' . $this->url->link('checkout/failure') . '">' . "\n";
+        echo '</head>' . "\n";
+        echo '<body>' . "\n";
+        echo '  <p>Loading... <a href="' . $this->url->link('checkout/failure') . '">link</a>!</p>' . "\n";
+        echo '</body>' . "\n";
+        echo '</html>' . "\n";
+        
+    }
+
+    public function return_ipn() {
+
+        $this->load->model('checkout/order');
+        $order_info = $this->model_checkout_order->getOrder($this->request->post['orderid']);
+        $vkey = $this->config->get('payment_molpay_skey');
+
+        $_POST['treq']=   1;
+        $tranID = (isset($_POST['tranID']) && !empty($_POST['tranID'])) ? $_POST['tranID'] : '';
+        $orderid = (isset($_POST['orderid']) && !empty($_POST['orderid'])) ? $_POST['orderid'] : '';
+        $status = (isset($_POST['status']) && !empty($_POST['status'])) ? $_POST['status'] : '';
+        $domain = (isset($_POST['domain']) && !empty($_POST['domain'])) ? $_POST['domain'] : '';
+        $amount = (isset($_POST['amount']) && !empty($_POST['amount'])) ? $_POST['amount'] : '';
+        $currency = (isset($_POST['currency']) && !empty($_POST['currency'])) ? $_POST['currency'] : '';
+        $appcode = (isset($_POST['appcode']) && !empty($_POST['appcode'])) ? $_POST['appcode'] : '';
+        $paydate = (isset($_POST['paydate']) && !empty($_POST['paydate'])) ? $_POST['paydate'] : '';
+        $skey = (isset($_POST['skey']) && !empty($_POST['skey'])) ? $_POST['skey'] : '';
+
+        /***********************************************************
+        * Backend acknowledge method for IPN (DO NOT MODIFY)
+        ************************************************************/         
+        foreach ($_POST as $k => $v) {
+          $postData[]= $k."=".$v;
+        }      
+        $postdata   = implode("&",$postData);
+        $url        = $this->config->get('payment_molpay_type')."MOLPay/API/chkstat/returnipn.php";
+        $ch         = curl_init();
+        curl_setopt($ch, CURLOPT_POST           , 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS     , $postdata);
+        curl_setopt($ch, CURLOPT_URL            , $url);
+        curl_setopt($ch, CURLOPT_HEADER         , 1);
+        curl_setopt($ch, CURLINFO_HEADER_OUT    , TRUE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , FALSE);
+        //curl_setopt($ch, CURLOPT_SSLVERSION     , 3);
+        $result = curl_exec( $ch );
+        curl_close( $ch );
+        /***********************************************************
+        * End of Acknowledge method for IPN
+        ************************************************************/
+
+        $key0 = md5($tranID.$orderid.$status.$domain.$amount.$currency);
+        $key1 = md5($paydate.$domain.$key0.$appcode.$vkey);
+
+        if ( $skey != $key1 )
+            $status = -1 ;
+            $order_status_id = $this->config->get('config_order_status_id');
+        if ( $status == "00" ) {
+            $order_status_id = $this->config->get('payment_molpay_completed_status_id');
+        } elseif( $status == "22" ) {
+            $order_status_id = $this->config->get('payment_molpay_pending_status_id');
+        } else {
+            $order_status_id = $this->config->get('payment_molpay_failed_status_id');
+            unset($this->session->data['language']);
+            unset($this->session->data['order_id']);
+        }
+
+        $param = "";
+        foreach ($_POST As $kP => $vP) {
+            $param .= $kP . "=" . $vP . "<br>";
+        }
+
+        $successStatus = array('00','22');
+
+        if (in_array($status, $successStatus))  // replace this line with if($status=='00') if only want to update status when success payment
+            $this->model_checkout_order->addOrderHistory($orderid, $order_status_id, $param);
+
+        if (in_array($status,$successStatus)) {
+            echo '<html>' . "\n";
+            echo '<head>' . "\n";
+            echo '  <meta http-equiv="Refresh" content="0; url=' . $this->url->link('checkout/success') . '">' . "\n";
+            echo '</head>' . "\n";
+            echo '<body>' . "\n";
+            echo '  <p>Please follow <a href="' . $this->url->link('checkout/success') . '">link</a>!</p>' . "\n";
+            echo '</body>' . "\n";
+            echo '</html>' . "\n";
+        } else {
+            echo '<html>' . "\n";
+            echo '<head>' . "\n";
+            echo '  <meta http-equiv="Refresh" content="0; url=' . $this->url->link('checkout/failure') . '">' . "\n";
+            echo '</head>' . "\n";
+            echo '<body>' . "\n";
+            echo '  <p>Please follow <a href="' . $this->url->link('checkout/failure') . '">link</a>!</p>' . "\n";
+            echo '</body>' . "\n";
+            echo '</html>' . "\n";
+        }
+        exit();
+    }
+
+    /*****************************************************
+    * Callback with IPN(Instant Payment Notification)
+    ******************************************************/
+    public function callback_ipn()   {
+        $this->load->model('checkout/order');
+
+        $vkey = $this->config->get('payment_molpay_skey');
+
+        $nbcb = (isset($_POST['nbcb']) && !empty($_POST['nbcb'])) ? $_POST['nbcb'] : '';
+        $tranID = (isset($_POST['tranID']) && !empty($_POST['tranID'])) ? $_POST['tranID'] : '';
+        $orderid = (isset($_POST['orderid']) && !empty($_POST['orderid'])) ? $_POST['orderid'] : '';
+        $status = (isset($_POST['status']) && !empty($_POST['status'])) ? $_POST['status'] : '';
+        $domain = (isset($_POST['domain']) && !empty($_POST['domain'])) ? $_POST['domain'] : '';
+        $amount = (isset($_POST['amount']) && !empty($_POST['amount'])) ? $_POST['amount'] : '';
+        $currency = (isset($_POST['currency']) && !empty($_POST['currency'])) ? $_POST['currency'] : '';
+        $appcode = (isset($_POST['appcode']) && !empty($_POST['appcode'])) ? $_POST['appcode'] : '';
+        $paydate = (isset($_POST['paydate']) && !empty($_POST['paydate'])) ? $_POST['paydate'] : '';
+        $skey = (isset($_POST['skey']) && !empty($_POST['skey'])) ? $_POST['skey'] : '';
+
+        $key0 = md5($tranID.$orderid.$status.$domain.$amount.$currency);
+        $key1 = md5($paydate.$domain.$key0.$appcode.$vkey);
+
+        if ( $skey != $key1 )
+            $status = -1 ;
+
+        if ($nbcb == 1) {
+            echo "CBTOKEN:MPSTATOK";
+
+            $order_info = $this->model_checkout_order->getOrder($this->request->post['orderid']); 
+            $order_status_id = $this->config->get('config_order_status_id');
+
+            if ( $status == "00" )  {
+                $order_status_id = $this->config->get('payment_molpay_completed_status_id');
+            } elseif( $status == "22" ) {
+                $order_status_id = $this->config->get('payment_molpay_pending_status_id');
+            } else {
+                $order_status_id = $this->config->get('payment_molpay_failed_status_id');
+            }
+
+            $param = "";
+            foreach ($_POST As $kP => $vP) {
+                $param .= $kP . "=" . $vP . "<br>";
+            }
+
+
+            $successStatus = array('00','22');
+
+            if (in_array($status, $successStatus))  // replace this line with if($status=='00') if only want to update status when success payment
+                $this->model_checkout_order->addOrderHistory($orderid, $order_status_id, $param);
+        }
+    }
+
+    /*****************************************************
+    * Notification with IPN(Instant Payment Notification)
+    ******************************************************/
+    public function notification_ipn()   {
+        $this->load->model('checkout/order');
+
+        $vkey = $this->config->get('payment_molpay_skey');
+
+        $nbcb = (isset($_POST['nbcb']) && !empty($_POST['nbcb'])) ? $_POST['nbcb'] : '';
+        $tranID = (isset($_POST['tranID']) && !empty($_POST['tranID'])) ? $_POST['tranID'] : '';
+        $orderid = (isset($_POST['orderid']) && !empty($_POST['orderid'])) ? $_POST['orderid'] : '';
+        $status = (isset($_POST['status']) && !empty($_POST['status'])) ? $_POST['status'] : '';
+        $domain = (isset($_POST['domain']) && !empty($_POST['domain'])) ? $_POST['domain'] : '';
+        $amount = (isset($_POST['amount']) && !empty($_POST['amount'])) ? $_POST['amount'] : '';
+        $currency = (isset($_POST['currency']) && !empty($_POST['currency'])) ? $_POST['currency'] : '';
+        $appcode = (isset($_POST['appcode']) && !empty($_POST['appcode'])) ? $_POST['appcode'] : '';
+        $paydate = (isset($_POST['paydate']) && !empty($_POST['paydate'])) ? $_POST['paydate'] : '';
+        $skey = (isset($_POST['skey']) && !empty($_POST['skey'])) ? $_POST['skey'] : '';
+
+        $key0 = md5($tranID.$orderid.$status.$domain.$amount.$currency);
+        $key1 = md5($paydate.$domain.$key0.$appcode.$vkey);
+
+        if ( $skey != $key1 )
+            $status = -1 ;
+
+        if ($nbcb == 2) {
+            echo "CBTOKEN:MPSTATOK";
+            $order_info = $this->model_checkout_order->getOrder($this->request->post['orderid']);
+            //orderid
+
+            $order_status_id = $this->config->get('config_order_status_id');
+
+            if ( $status == "00" ) {
+                $order_status_id = $this->config->get('payment_molpay_completed_status_id');
+            } elseif( $status == "22" ) {
+                $order_status_id = $this->config->get('payment_molpay_pending_status_id');
+            } else {
+                $order_status_id = $this->config->get('payment_molpay_failed_status_id');
+            }
+                        
+            $param = "";
+            foreach ($_POST As $kP => $vP) {
+                $param .= $kP . "=" . $vP . "<br>";
+            }
+
+            $successStatus = array('00','22');
+
+            if (in_array($status, $successStatus))  // replace this line with if($status=='00') if only want to update status when success payment
+                $this->model_checkout_order->addOrderHistory($orderid, $order_status_id, $param);
+        }
+    }
+}
+
